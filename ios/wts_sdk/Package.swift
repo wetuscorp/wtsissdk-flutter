8a0abf2d670edc24db80b0a017f15569e91a891f
Gülali Çelik <gulalicelikmi@gmail.com>
2026-07-12 17:12:58 +0300

    fix: validate Flutter iOS 15 SPM and CocoaPods

--- ios/wts_sdk/Package.swift
+++ ios/wts_sdk/Package.swift
@@ -1,20 +1,30 @@
 // swift-tools-version: 5.9
 import PackageDescription
+import Foundation
+
+let nativeSdkPath = ProcessInfo.processInfo.environment["WTS_SDK_SWIFT_PATH"]
+let nativeSdkDependency: Package.Dependency = if let nativeSdkPath {
+    .package(name: "wtsissdk-swift", path: nativeSdkPath)
+} else {
+    .package(
+        url: "https://github.com/wetuscorp/wtsissdk-swift.git",
+        exact: "0.1.0-alpha.1"
+    )
+}
 
 let package = Package(
     name: "wts_sdk",
     platforms: [.iOS(.v15)],
     products: [.library(name: "wts-sdk", targets: ["wts_sdk"])],
     dependencies: [
-        .package(
-            url: "https://github.com/wetuscorp/wtsissdk-swift.git",
-            exact: "0.1.0-alpha.1"
-        )
+        .package(name: "FlutterFramework", path: "../FlutterFramework"),
+        nativeSdkDependency,
     ],
     targets: [
         .target(
             name: "wts_sdk",
             dependencies: [
+                .product(name: "FlutterFramework", package: "FlutterFramework"),
                 .product(name: "WtsSDK", package: "wtsissdk-swift")
             ]
         )
